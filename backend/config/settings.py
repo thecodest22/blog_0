720b28f90ee9490417d3064ff514a5bee8549052
codest <thecodest@ya.ru>
2024-05-29 22:03:31 +0300

    Jinja2 added

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -49,6 +49,14 @@ ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
     {
+        'BACKEND': 'django.template.backends.jinja2.Jinja2',
+        'DIRS': [BASE_DIR / 'templates'],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'environment': 'blog.jinja2.environment',
+        }
+    },
+    {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
