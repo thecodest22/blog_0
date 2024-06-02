542881fc75a3255accbd1c5cfc51c5604c6bc411
codest <thecodest@ya.ru>
2024-06-02 23:22:28 +0300

    Fonts applied

--- backend/tailwind.config.js
+++ backend/tailwind.config.js
@@ -1,12 +1,24 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: [
       './src/**/*.{css,js}',
       './templates/*.html',
-      './**/templates/*.html',
+      './**/templates/**/*.html',
+      './**/jinja2/**/*.html',
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        xs: '320px'
+      },
+      fontFamily: {
+        'sans': ['"Montserrat Alternates"', ...defaultTheme.fontFamily.sans],
+        // 'Cormorant': ['"Cormorant Infant"', "sans-serif"],
+        'serif': ['"Cormorant Infant"', ...defaultTheme.fontFamily.serif],
+      },
+    },
   },
   plugins: [],
 }
